5cd6a2a46a932085387cdb0762a658117a09d9a5
Benny van Zuiden <zuiden@lorentz.leidenuniv.nl>
2016-03-10 13:18:30 +0100

    Added functionality to change header adn wrap directives

--- tlv.h
+++ tlv.h
@@ -23,51 +23,66 @@ namespace tlv
     struct svg
     {
         double width,height;
+        std::string header;
         std::vector<std::string> data;
-        svg()
-        {
-            width=height=1e3;
-        }
-        svg(double x)
-        {
-            width=height=x;
-        }
-        svg(double w,double h)
-        {
-            width=w,height=h;
-        }
-        bool write(std::string fout_name)
-        {
-            FILE *fout=fopen(fout_name.c_str(),"w");
-            if(fout)
-            {
-                const char header[]="<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%g\" height=\"%g\">\n";
-                fprintf(fout,header,width,height);
-                for(std::string str: data) fprintf(fout,"%s\n",str.c_str());
-                fprintf(fout,"</svg>\n");
-                fclose(fout);
-                return true;
-            }
-            else return false;
-        }
-        void prepend(svg *img)
-        {
-            data.insert(data.begin(),img->data.begin(),img->data.end());
-        }
-        void append(svg *img)
-        {
-            data.insert(data.end(),img->data.begin(),img->data.end());
-        }
-        void import(svg *img)
-        {
-            data=img->data;
-        }
-        void clear()
-        {
-            data.clear();
-        }
+        svg();
+        svg(double x);
+        svg(double w,double h);
+        bool write(std::string fout_name);
+        void prepend(svg *img);
+        void append(svg *img);
+        void import(svg *img);
+        void clear();
     };
-    template<typename ...Args> bool tlv(svg *img,std::string str,Args... args)
+    template<typename ...Args> bool tlv_drw(svg *img,std::string str,Args... args);
+    template<typename ...Args> bool tlv_hdr(svg *img,std::string str,Args... args);
+    template<typename ...Args> bool tlv_yfo(svg *img,std::string marker,std::string content,std::string str,Args... args);
+    template<typename ...Args,typename lambda> bool tlv_for(svg *img,std::string marker,lambda x,std::string str,Args... args);
+    svg::svg()
+    {
+        width=height=1e3;
+        tlv_hdr(this,"width=\"%g\" height=\"%g\"",width,height);
+    }
+    svg::svg(double x)
+    {
+        width=height=x;
+        tlv_hdr(this,"width=\"%g\" height=\"%g\"",width,height);
+    }
+    svg::svg(double w,double h)
+    {
+        width=w,height=h;
+        tlv_hdr(this,"width=\"%g\" height=\"%g\"",width,height);
+    }
+    bool svg::write(std::string fout_name)
+    {
+        FILE *fout=fopen(fout_name.c_str(),"w");
+        if(fout)
+        {
+            fprintf(fout,"%s\n",header.c_str());
+            for(std::string str: data) fprintf(fout,"%s\n",str.c_str());
+            fprintf(fout,"</svg>\n");
+            fclose(fout);
+            return true;
+        }
+        else return false;
+    }
+    void svg::prepend(svg *img)
+    {
+        data.insert(data.begin(),img->data.begin(),img->data.end());
+    }
+    void svg::append(svg *img)
+    {
+        data.insert(data.end(),img->data.begin(),img->data.end());
+    }
+    void svg::import(svg *img)
+    {
+        data=img->data;
+    }
+    void svg::clear()
+    {
+        data.clear();
+    }
+    template<typename ...Args> bool tlv_drw(svg *img,std::string str,Args... args)
     {
         const std::string svd="<"+str+"/>";
         const size_t size=1+snprintf(nullptr,0,svd.c_str(),args...);
@@ -77,6 +92,20 @@ namespace tlv
         {
             snprintf(buffer,size,svd.c_str(),args...);
             img->data.push_back(std::string(buffer));
+            delete[] buffer;
+            return true;
+        }
+    }
+    template<typename ...Args> bool tlv_hdr(svg *img,std::string str,Args... args)
+    {
+        const std::string svd="<svg xmlns=\"http://www.w3.org/2000/svg\" "+str+">";
+        const size_t size=1+snprintf(nullptr,0,svd.c_str(),args...);
+        char *buffer=new(std::nothrow) char[size];
+        if(buffer==nullptr) return false;
+        else
+        {
+            snprintf(buffer,size,svd.c_str(),args...);
+            img->header=std::string(buffer);
             delete[] buffer;
             return true;
         }
@@ -91,6 +120,22 @@ namespace tlv
         {
             snprintf(buffer,size,svd.c_str(),args...);
             img->data.push_back(std::string(buffer)+content+std::string("</")+marker+std::string(">"));
+            delete[] buffer;
+            return true;
+        }
+    }
+    template<typename ...Args> bool tlv_obj(svg *img,std::string marker,svg *object,std::string str,Args... args)
+    {
+        const std::string svd="<"+marker+" "+str+">";
+        const size_t size=1+snprintf(nullptr,0,svd.c_str(),args...);
+        char *buffer=new(std::nothrow) char[size];
+        if(buffer==nullptr) return false;
+        else
+        {
+            snprintf(buffer,size,svd.c_str(),args...);
+            img->data.push_back(std::string(buffer));
+            img->append(object);
+            img->data.push_back(std::string("</")+marker+std::string(">"));
             delete[] buffer;
             return true;
         }
@@ -112,7 +157,8 @@ namespace tlv
         }
     }
 }
-#define tlv_drw(img,str,...) tlv::tlv(img,#str,##__VA_ARGS__)
+#define tlv_drw(img,str,...) tlv::tlv_drw(img,#str,##__VA_ARGS__)
+#define tlv_hdr(img,str,...) tlv::tlv_hdr(img,#str,##__VA_ARGS__)
 #define tlv_yfo(img,mrk,txt,str,...) tlv::tlv_yfo(img,mrk,txt,#str,##__VA_ARGS__)
 #define tlv_for(img,mrk,lambda,str,...) tlv::tlv_for(img,mrk,lambda,#str,##__VA_ARGS__)
 #endif
