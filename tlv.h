74ae86fb3ec800d26a7e7b0a6d5d66d9091d3058
Benny van Zuiden <zuiden@lorentz.leidenuniv.nl>
2016-03-10 14:10:43 +0100

    Added functionality to open and close directives

--- tlv.h
+++ tlv.h
@@ -34,8 +34,10 @@ namespace tlv
         void import(svg *img);
         void clear();
     };
-    template<typename ...Args> bool tlv_drw(svg *img,std::string str,Args... args);
     template<typename ...Args> bool tlv_hdr(svg *img,std::string str,Args... args);
+    template<typename ...Args> bool tlv_drw(svg *img,std::string str,Args... args);
+    template<typename ...Args> bool tlv_opn(svg *img,std::string str,Args... args);
+    bool tlv_cls(svg *img,std::string str);
     template<typename ...Args> bool tlv_yfo(svg *img,std::string marker,std::string content,std::string str,Args... args);
     template<typename ...Args> bool tlv_obj(svg *img,std::string marker,svg *object,std::string str,Args... args);
     template<typename ...Args,typename lambda> bool tlv_for(svg *img,std::string marker,lambda x,std::string str,Args... args);
@@ -83,6 +85,20 @@ namespace tlv
     {
         data.clear();
     }
+    template<typename ...Args> bool tlv_hdr(svg *img,std::string str,Args... args)
+    {
+        const std::string svd="<svg xmlns=\"http://www.w3.org/2000/svg\" "+str+">";
+        const size_t size=1+snprintf(nullptr,0,svd.c_str(),args...);
+        char *buffer=new(std::nothrow) char[size];
+        if(buffer==nullptr) return false;
+        else
+        {
+            snprintf(buffer,size,svd.c_str(),args...);
+            img->header=std::string(buffer);
+            delete[] buffer;
+            return true;
+        }
+    }
     template<typename ...Args> bool tlv_drw(svg *img,std::string str,Args... args)
     {
         const std::string svd="<"+str+"/>";
@@ -97,19 +113,25 @@ namespace tlv
             return true;
         }
     }
-    template<typename ...Args> bool tlv_hdr(svg *img,std::string str,Args... args)
+    template<typename ...Args> bool tlv_opn(svg *img,std::string str,Args... args)
     {
-        const std::string svd="<svg xmlns=\"http://www.w3.org/2000/svg\" "+str+">";
+        const std::string svd="<"+str+">";
         const size_t size=1+snprintf(nullptr,0,svd.c_str(),args...);
         char *buffer=new(std::nothrow) char[size];
         if(buffer==nullptr) return false;
         else
         {
             snprintf(buffer,size,svd.c_str(),args...);
-            img->header=std::string(buffer);
+            img->data.push_back(std::string(buffer));
             delete[] buffer;
             return true;
         }
+    }
+    bool tlv_cls(svg *img,std::string str)
+    {
+        const std::string svd="</"+str+">";
+        img->data.push_back(svd);
+        return true;
     }
     template<typename ...Args> bool tlv_yfo(svg *img,std::string marker,std::string content,std::string str,Args... args)
     {
@@ -158,8 +180,10 @@ namespace tlv
         }
     }
 }
-#define tlv_drw(img,str,...) tlv::tlv_drw(img,#str,##__VA_ARGS__)
 #define tlv_hdr(img,str,...) tlv::tlv_hdr(img,#str,##__VA_ARGS__)
+#define tlv_drw(img,str,...) tlv::tlv_drw(img,#str,##__VA_ARGS__)
+#define tlv_opn(img,str,...) tlv::tlv_opn(img,#str,##__VA_ARGS__)
+#define tlv_cls(img,str,...) tlv::tlv_cls(img,#str,##__VA_ARGS__)
 #define tlv_yfo(img,mrk,txt,str,...) tlv::tlv_yfo(img,mrk,txt,#str,##__VA_ARGS__)
 #define tlv_obj(img,mrk,object,str,...) tlv::tlv_for(img,mrk,object,#str,##__VA_ARGS__)
 #define tlv_for(img,mrk,lambda,str,...) tlv::tlv_for(img,mrk,lambda,#str,##__VA_ARGS__)
